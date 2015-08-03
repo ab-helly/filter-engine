316cdcc49936c22212e6b0b077abae9e91b06124
Olga Vshivtseva <olga.vshivtseva@rebelmouse.com>
2015-08-02 18:54:09 -0700

    Initial commit

---
+++ app/src/main/java/me/abhelly/filterengine/FilterFragment.java
@@ -0,0 +1,22 @@
+package me.abhelly.filterengine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FilterFragment extends Fragment {
+
+    public FilterFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_filter, container, false);
+    }
+}
